71452e362109bb24d164681ace0af6de249b8ea8
t0268664 <ivelina.stoyanova@thalesgroup.com>
2022-06-13 15:02:31 +0200

    added dummy pattern

--- Elements/Pattern.py
+++ Elements/Pattern.py
@@ -1,2 +1,24 @@
+from Elements.Event import Event
+
+
+def parse_instruction():
+    symbol = ("A", [0, 1, 2])
+    duration = 2
+    return [(symbol, duration)]
+
+
 class Pattern:
-    satisfied = None
+
+    def __init__(self, instruction: list[tuple]):
+        self.instruction = instruction
+        self.instruction_buffer = []
+        self.satisfied = False
+
+    def sample(self):
+        current_instruction = self.instruction.pop(0)
+        self.instruction_buffer.append(current_instruction)
+        if self.instruction_buffer:
+            self.satisfied = True
+            self.instruction = self.instruction_buffer
+        return current_instruction
+
