df59fe049f8a421f8daa554de5cb879e5d3d8415
t0268664 <ivelina.stoyanova@thalesgroup.com>
2022-06-17 15:20:14 +0200

    add doc to box code + clean code TODO adapt rest of code

--- Elements/InteractiveBox.py
+++ Elements/InteractiveBox.py
@@ -4,33 +4,74 @@ from Elements.Pattern import Pattern
 class InteractiveBox:
     def __init__(self, id: int, pattern: Pattern = None):
         """
-        # TODO
-        :param id:
-        :param pattern:
+        An openable box that allows interaction. It possesses three states indicators: open, ready and active.
+        The box is initialised with a pattern which defines when the box changes states.
+
+        :param id: id of box from 0 to (nb_boxes - 1)
+        :param pattern: pattern object linked to box
         """
         self.id = id
-        self.box = { "open": False, "ready": False, "active": True}
+        self.box = {"open": False, "ready": False, "active": True}
         self.pattern = pattern
 
-    def press_button(self):
-        opened = False
-        if not self.box["open"]:
-            opened = self.box["active"] and self.box["ready"]
-            self.box["open"] = opened
-        return opened
+    def open(self):
+        """
+        Opens box, deactivates it once it is opened and marks it as not ready
+        """
+        self.box["open"] = True
+        self.box["ready"] = False
+        self.box["active"] = False
 
     def activate(self):
+        """
+        Activates box, marks it as not ready and not opened
+        """
         assert not self.box["open"], "Cannot activate an opened box"
         assert not self.box["ready"], "Newly activated boxes shouldn't be ready"
         self.box["active"] = True
+        self.box["ready"] = False
+        self.box["open"] = False
+
+    def deactivate(self):
+        """
+        Deactivates box, marks it as not ready and not open
+        """
+        self.box["active"] = False
+        self.box["ready"] = False
+        self.box["open"] = False
+
+    def ready(self):
+        """
+        Makes box ready to open, removes active status
+        """
+        self.box["active"] = False
+        self.box["ready"] = True
+        self.box["open"] = False
+
+    def check_pattern(self):
+        if self.pattern.satisfied:
+            self.ready()
+
+    def press_button(self):
+        """
+        Attempt to open box via button press. If box is active and ready it will open.
+        :return: success of attempt
+        """
+        if not self.box["open"]:  # if the box has not been opened already
+            if self.box["active"] and self.box["ready"]:  # if the box is active and ready to open
+                self.open()
+                return True
+        return False  # in all other cases return false
 
     def update(self):
+        """
+        Verifies bos state and updates its status
+        """
         if not self.box["open"]:
             if self.box["active"]:
                 # if the box has been ready it should be timed out
                 if self.box["ready"]:
-                    self.box["active"] = False
-                    self.box["ready"] = False
+                    self.deactivate()
                 # otherwise, check if pattern has been satisfied
                 else:
-                    self.box["ready"] = self.pattern.satisfied
+                    self.check_pattern()
