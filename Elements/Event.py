f94882f299f90e43e0b853193fbe3efd5cf1b58a
t0268664 <ivelina.stoyanova@thalesgroup.com>
2022-09-22 15:44:40 +0200

    adding doc for environment

--- Elements/Event.py
+++ Elements/Event.py
@@ -2,8 +2,6 @@ import math
 from copy import deepcopy
 
 
-# TODO doc (priority 4)
-
 class Event:
     def __init__(self,
                  e_type: str,
@@ -12,15 +10,25 @@ class Event:
                  t_end: float):
         # TODO (priority 2) reorganise event object structure all the dictionaries are awkward
         """
-        :param e_type:
-        :param e_attributes:
-        :param t_start:
-        :param t_end:
+        Define an event object with a type, attributes, a beginning and an end.
+
+        :param e_type: type describing event using string, usually just one character suffices for simpler environments
+        :param e_attributes: dictionary of attributes following the form {'attribute_name': 'attribute value'}
+        :param t_start: time of start of event
+        :param t_end: time of end of event
+
+        Note: Event end time should be superior to event beginning : t_end >= t_start
         """
         self.symbol = {"e_type": e_type, "attr": e_attributes}
         self.start = t_start
         self.end = t_end
         self.duration = t_end - t_start if (t_end != math.inf) else math.inf
+
+    def get_type(self):
+        return self.symbol["e_type"]
+
+    def get_attribute_val(self, attr):
+        return self.symbol["attr"][attr]
 
     def set_time(self, t_start, t_end):
         self.start = t_start
@@ -29,10 +37,13 @@ class Event:
 
     def shift(self, delta):
         """
+        Shift event start and end by a fixed value.
 
-        :param delta:
-        :return:
+        :param delta: Value by which to shift the event.
+        :return: Copy of event with new start and end times
         """
+        assert self.start + delta >= 0, f"Cannot start event sooner than 0, shifting by {delta} would lead to starting at {self.start + delta}"
+
         new = deepcopy(self)
         new.start += delta
         new.end += delta
@@ -59,7 +70,6 @@ class Event:
         return f"Event('e_type': {self.symbol['e_type']}, " \
                f"'attr': {self.symbol['attr']}, " \
                f"'start' : {self.start}, 'end': {self.end})"
-
 
     def __repr__(self):
         return str(self)
---
+++ examples/create_event.py
@@ -0,0 +1,5 @@
+from Elements.Event import Event
+
+e = Event("A", {"attr1": "blue", "attr2": "green"}, 5, 10)
+shifted_e = e.shift(10)
+shifted_e = e.shift(-5)
