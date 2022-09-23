365db9bcb00323bbbc7ba142efef59f459071068
t0268664 <ivelina.stoyanova@thalesgroup.com>
2022-09-23 15:11:59 +0200

    Parser doc

--- Dynamics/Parser.py
+++ Dynamics/Parser.py
@@ -19,14 +19,20 @@ class Parser:
                  all_event_attributes,
                  all_noise_attributes):
         """
-        Parser class that allows to generate event patterns using instructions.
-        :param all_event_types: list of all possible event types to be used in the environment.
-        :param all_event_attributes: dictionary of lists of all possible corresponding values for each event type.
+        Allows to process instructions and generate events and noise
+        on the base of a finite set of preexistent types and attributes.
+
+        :param all_event_types: list of all possible event types to be used for event generation
+        :param all_event_attributes: dictionary of all attributes and their associated possible values
+                                                                                for event generation
+        :param all_noise_types: list of all possible event types to be used for noise generation
+        :param all_noise_attributes: dictionary of attributes used for noise generation
         """
         self.all_event_types = all_event_types
         self.all_noise_types = all_noise_types
         self.all_event_attributes = all_event_attributes
         self.all_noise_attributes = all_noise_attributes
+        # to be used for event generation when no duration is specified
         self.min_max_durations = {"min": 1, "max": 1}
 
         self.all_types = all_event_types + all_noise_types
@@ -34,23 +40,29 @@ class Parser:
             self.all_attributes = {key: all_noise_attributes[key] + all_event_attributes[key]
                                    for key in all_event_attributes}
         else:
-            self.all_attributes = {key: all_event_attributes[key]
-                                   for key in all_event_attributes}
+            self.all_attributes = all_event_attributes
 
     def labelise(self, e_type: str, attributes: dict):
         """
-        Associates a label to an event type and a dictionary of attributes.
-        In this way we make sure that all associated labels are coherent.
+        Transform an event type and a dictionary of attribute values into
+        an integer label for the type and a dictionary of attributes with integer label values.
 
         :param e_type: The event type to be transformed into an integer label.
         :param attributes: The dictionary of attributes to transform. Each value becomes an integer label while parameter names stay unchanged.
-        :return: Integer label type and dictionary of integer value attributes.
+        :return: Integer label type and dictionary of attributes with integer label values.
         """
         e_type = self.all_types.index(e_type)
         attributes = {key: self.all_attributes[key].index(value) for key, value in attributes.items()}
         return e_type, attributes
 
-    def noise(self, before):
+    def make_noise(self, before):
+        """
+        Generate a random noise event ending before a certain date.
+        The types and attributes of this event are taken from the sets given at class initialisation
+
+        :param before: Time before which the noise should be generated
+        :return: the defined noise event
+        """
         t1, t2 = random.uniform(0, before), random.uniform(0, before)
         start, end = min(t1, t2), max(t1, t2)
         e_type = random.choice(self.all_noise_types)
@@ -58,14 +70,11 @@ class Parser:
         for attr, attr_values in self.all_noise_attributes.items():
             if attr not in attributes:
                 attributes[attr] = random.choice(attr_values)
-        # TODO (priority 3) move somewhere else repeats in instantiate
-        e_type, attributes = self.labelise(e_type, attributes)
-        noise = Event(e_type, attributes, 0, 1)
-        noise.set_time(start, end)
+        noise = self.instantiate_with_labels(e_type, attributes, start, end)
         return noise
 
     # TODO (priority 3) add function to check if values are correct
-    def instantiate(self, e_type: str = None, attributes: dict = {}, duration_distribution: dict() = {}):
+    def make_event(self, e_type: str = None, attributes: dict = {}, duration_distribution: dict() = {}):
         """
         Instantiates an event of given parameters of duration drawn according to a truncated normal distribution.
         If event parameters or type are not specified they are randomly drawn from the list of available ones.
@@ -89,16 +98,40 @@ class Parser:
             duration_distribution = self.get_random_duration_dist()
 
         duration_inst = my_normal(**duration_distribution)
+        event = self.instantiate_with_labels(e_type, attributes, 0, duration_inst)
+        return event
+
+    def instantiate_with_labels(self, e_type, attributes, start, end):
+        """
+        Transform type and attributes to labels and instantiate Event object.
+
+        :param e_type: the type of the event/noise
+        :param attributes: the attributes
+        :param start: the beginning of the event
+        :param end: the end of the event
+        :return: an event object with the given information transformed into labels
+        """
         e_type, attributes = self.labelise(e_type, attributes)
-        return Event(e_type, attributes, 0, duration_inst)
+        noise = Event(e_type, attributes, start, end)
+        return noise
 
     def record_duration(self, duration):
-        if duration < self.min_max_durations["min"]:
-            self.min_max_durations["min"] = duration
-        elif duration > self.min_max_durations["max"]:
-            self.min_max_durations["max"] = duration
+        """
+        Update the records of smallest observed and largest observed durations.
+        This is used for generating events that have no specified duration.
 
+        :param duration: The duration to update the values with.
+        """
+        self.min_max_durations["min"] = min(duration, self.min_max_durations["min"])
+        self.min_max_durations["max"] = max(duration, self.min_max_durations["max"])
+
+    # TODO (priority 2) this can be done better
     def get_random_duration_dist(self):
+        """
+        Get a random duration distribution based on the saved statistics.
+
+        :return: A dictionary giving the mu and sigma of the new distributions
+        """
         sigma = (self.min_max_durations["max"] - self.min_max_durations["min"]) / 2
         mu = self.min_max_durations["min"] + sigma
         return {"mu": mu, "sigma": sigma}
@@ -106,16 +139,16 @@ class Parser:
     def generate_pattern(self, instructions: dict):
         """
         Generated a pattern of events using a dictionary of commands following a particular format.
+        To see exact format refer to examples.instructions.
 
         :param instructions: The dictionary of commands.
         :return: A list of events that follows the selected instructions.
         """
         event_list = []
-        # TODO (priority 3) make same for all parser however problem with same names over configs
         variables = dict()
         for instr_line in instructions:
             if instr_line["command"] == "instantiate":
-                event = self.instantiate(*instr_line["parameters"])
+                event = self.make_event(*instr_line["parameters"])
                 variables[instr_line["variable_name"]] = event
                 if not event_list:
                     event_list.append(event)
@@ -127,6 +160,6 @@ class Parser:
                 variables[instr_line["variable_name"]] = event
                 event_list.append(event)
             else:
-                # TODO (priority 3) add more errors and tests for correctly used values
+                # TODO (priority 1) add more errors and tests for correctly used values
                 raise ValueError("Unknown allen command: " + str(instr_line["command"]))
         return sorted(variables.values())
---
+++ examples/parser_usage.py
@@ -0,0 +1,27 @@
+from Dynamics.Parser import Parser
+from examples.create_env import all_event_attributes, all_noise_attributes, all_event_types, all_noise_types
+from examples.instructions import instructions
+
+p = Parser(all_event_types, all_noise_types, all_event_attributes, all_noise_attributes)
+
+example_labelised = p.labelise("A", {"fg": "blue"})
+example_labelised = p.labelise("A", {"fg": "blue", "bg": "red"})
+
+# ignore delay and noise commands
+event_list = p.generate_pattern(instructions[0][2:])
+
+bad_instructions = [{'command': 'instantiate', 'parameters': ('xxx', {'bg': 'blue', 'fg': 'blue'}, {'mu': 5, 'sigma': 2}),
+                     'variable_name': 'distinct'},
+                    {'command': 'instantiate',
+                     'parameters': ('E', {'bg': 'pink', 'fg': 'green'}, {'mu': 6, 'sigma': 2}), 'variable_name': 'e1'},
+                    {'command': 'instantiate', 'parameters': ('E', {'bg': 'red', 'fg': 'green'}, {'mu': 8, 'sigma': 0}),
+                     'variable_name': 'e2'},
+                    {'command': 'instantiate',
+                     'parameters': ('C', {'bg': 'orange', 'fg': 'red'}, {'mu': 5, 'sigma': 1}), 'variable_name': 'c1'},
+                    {'command': 'met_by', 'parameters': ['e2', 'c1'], 'variable_name': 'e2', 'other': {}},
+                    {'command': 'during', 'parameters': ['e1', 'e2'], 'variable_name': 'e1', 'other': {}},
+                    {'command': 'after', 'parameters': ['distinct', 'e2'], 'variable_name': 'distinct',
+                     'other': {'gap_dist': {'mu': 4, 'sigma': 1}}}]
+
+
+p.generate_pattern(bad_instructions)
