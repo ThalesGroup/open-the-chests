54aea829d12c4af015a1ddebc881b072f9a97572
t0268664 <ivelina.stoyanova@thalesgroup.com>
2022-09-27 17:38:06 +0200

    some more re-arragning and adding a proper main file

--- main.py
+++ main.py
@@ -1,130 +1,69 @@
-import random
+import argparse
 
-import plotly.io as pio
-from stable_baselines3 import DQN
-
+from stable_baselines3.common import results_plotter
 from stable_baselines3.common.monitor import Monitor
-
-from excog_experiments.Process import param_prepare
-from globals import ENV_CONFIG_FOLDER
 from src.BoxEventEnv import BoxEventEnv
+from models import a2c_model, dqn_model, ppo_model, trpo_model
 
-pio.renderers.default = "browser"
+available_models = ["A2C", "DQN", "PPO", "TRPO"]
 
-
-# TODO (priority 2) add seed to environment
+parser = argparse.ArgumentParser(description='Run a demo using a environment loaded via a configuration folder.')
+parser.add_argument('--env_config', type=str, default='one_per_box.yaml',
+                    help='The name of configuration file that defines an environment configuration.')
+parser.add_argument("--discrete", type=bool, default=False,
+                    help='Use discrete action input instead of a vector of binary values.')
+parser.add_argument("--model", type=str, default="TRPO",
+                    help=f'Model used to train on the environment. Current options : {available_models}')
+parser.add_argument("--verbose_train", type=bool, default=True,
+                    help='Use verbose mode when training model to get feedback.')
+parser.add_argument("--train_steps", type=int, default=100000,
+                    help="Number of steps to train model.")
+parser.add_argument("--plot_training", type=bool, default=True,
+                    help="Plot reward per step after training.")
 
 if __name__ == '__main__':
-    # conf = "multiple_per_box.yaml"
-    # conf = "one_distinct_per_box.yaml"
-    conf = "one_per_box.yaml"
-    env = BoxEventEnv.from_config_file(ENV_CONFIG_FOLDER + conf, False)
-    discrete_env = BoxEventEnv.from_config_file(ENV_CONFIG_FOLDER + conf, False, True)
-    verbose_env = BoxEventEnv.from_config_file(ENV_CONFIG_FOLDER + conf, True)
+    args = parser.parse_args()
+    print(args)
 
-    # env = Monitor(env, "deletethis")
-    discrete_env = Monitor(discrete_env, "")
+    # get config from parser
+    conf = args.env_config
+    assert conf.split(".")[-1] == "yaml", f"Config file {conf} must be yaml file."
+    # load environment from config
+    """
+    ATTENTION! 
+    Pattern instructions must be in the same place as main yaml.
+    The instructions must be in a folder with the same name.
+    """
+    discrete = args.discrete
+    env = BoxEventEnv.from_config_file("./config/" + conf,
+                                       verbose=False,
+                                       discrete=discrete)
 
-    # Train the agent
-    print("Learning")
+    # wrap env with monitor wrapper to track training
+    env = Monitor(env, "./results/")
 
-    # TRPO
-    # params = {'batch_size': 64,
-    #           'n_critic_updates': 25,
-    #           'cg_max_steps': 25,
-    #           'target_kl': 0.005,
-    #           'gamma': 0.98,
-    #           'gae_lambda': 0.8,
-    #           'n_steps': 64,
-    #           'learning_rate': 0.5039112109375,
-    #           'net_arch': 'small',
-    #           'activation_fn': 'tanh',
-    #           'seed': 42}
+    # get model to use for training
+    model_name = args.model
+    verbose_train = args.verbose_train
+    assert model_name in available_models, \
+        f"The selected model must be from the list {available_models}"
 
+    # load model depending on selection
+    if model_name == "A2C":
+        model = a2c_model.get_model(env=env, verbose=verbose_train)
+    elif model_name == "DQN":
+        assert discrete, "DQN model accepts only discrete actions, please set discrete parameter to True."
+        model = dqn_model.get_model(env=env, verbose=verbose_train)
+    elif model_name == "TRPO":
+        model = trpo_model.get_model(env=env, verbose=verbose_train)
+    elif model_name == "PPO":
+        model = ppo_model.get_model(env=env, verbose=verbose_train)
 
+    # get train steps
+    train_steps = args.train_steps
+    model.learn(train_steps)
 
-    #A2C
-    params = {'ent_coef': 0.011035165146484,
-              "vf_coef" : 0.39404296875,
-              "gamma" : 0.95,
-              "gae_lambda" : 0.98,
-              "n_steps": 2048,
-              "learning_rate": 0.29883513671875,
-              "net_arch" : "small",
-              "activation_fn": "relu",
-              "seed": 666,
-              "normalize_advantage": True,
-              "max_grad_norm": 0.3,
-              "use_rms_prop": False
-              }
-
-    activation_fn, learning_rate, net_arch = param_prepare(params["activation_fn"], params["learning_rate"], params["net_arch"])
-    for key in ["activation_fn", "learning_rate", "net_arch"]:
-        params.pop(key)
-
-    # model = TRPO('MultiInputPolicy', env, verbose=1,
-    #              batch_size=params["batch_size"],
-    #              seed=params["seed"],
-    #              n_steps=params["n_steps"],
-    #              gamma=params["gamma"],
-    #              learning_rate=learning_rate,
-    #              n_critic_updates=params["n_critic_updates"],
-    #              cg_max_steps=params["cg_max_steps"],
-    #              target_kl=params["target_kl"],
-    #              gae_lambda=params["gae_lambda"],
-    #              policy_kwargs=dict(
-    #                  net_arch=net_arch,
-    #                  ortho_init=0.5,
-    #                  activation_fn=activation_fn)
-    #              )
-    #
-    # model.learn(10000)
-    #
-    # bug_print(evaluate_multiple_times(env, model))
-
-    print("Evaluating")
-    # TODO (priority 1) give timeout to avoid infinite run
-    # print(evaluate_policy(model, verbose_env, n_eval_episodes=2))
-
-    # model = A2C('MultiInputPolicy', env, verbose=1,
-    #             policy_kwargs=dict(
-    #                 net_arch=net_arch,
-    #                 ortho_init=0.5,
-    #                 activation_fn=activation_fn),
-    #             **params
-    # ).learn(10000)
-
-    print("here")
-    model = DQN('MultiInputPolicy', discrete_env, verbose=1).learn(100000)
-    print("here")
-
-    # model = A2C('MultiInputPolicy', env, verbose=1)
-
-    # Test the trained agent
-
-    n_steps = 200
-    # verbose_env = first_box_env
-    print("------------------------ START -------------------------")
-    obs = discrete_env.reset()
-    discrete_env.render()
-
-    counter = 0
-    for step in range(n_steps):
-        counter += 1
-        action, _ = model.predict(obs, deterministic=True)
-        num_boxes = verbose_env.env.num_boxes
-        sure_action = [1] * num_boxes
-        empty_action = [0] * num_boxes
-        random_action = [[random.randint(0, 1) for i in range(num_boxes)]]
-        # print("Step {}".format(step + 1))
-        # print("Action: ", action)
-        obs, reward, done, info = discrete_env.step(action)
-        discrete_env.render()
-        # print('obs =', obs, 'reward=', reward, 'done=', done)
-        if done:
-            # Note that the VecEnv resets automatically
-            # when a done signal is encountered
-            print("Goal reached!", "reward=", reward)
-            break
-    print("here")
-    print(counter)
+    # plot results of training
+    if args.plot_training:
+        results_plotter.plot_results(["./results/"], 1e5, results_plotter.X_TIMESTEPS,
+                                     "Reward per time-step")
